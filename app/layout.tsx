7ae8172e3abe3318c76943288d2de55f7f61f887
astroupia <103548612+astroupia@users.noreply.github.com>
2024-12-20 13:13:50 +0300

    feat: admin page design udpate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { Footer } from '@/components/layout/footer';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Footer } from "@/components/layout/footer";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Liyu Catering',
-  description: 'Exceptional catering services for all occasions',
+  title: "Liyu Catering",
+  description: "Exceptional catering services for all occasions",
 };
 
 export default function RootLayout({
