28bb95d6312adaf9e69f8dfee6230accec75c0fd
HanaTeshome721 <hana2729tm@gmail.com>
2025-08-15 14:55:56 +0300

    feat: add ImageSlider component for enhanced hero section visuals

---
+++ components/home/imageslider.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import Hero1 from "@/public/assets/images/h1.png";
+import Hero2 from "@/public/assets/images/h3.jpeg";
+import Hero3 from "@/public/assets/images/h4.jpeg";
+import Hero10 from "@/public/assets/images/h55.jpeg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules"; 
+import "swiper/css";
+import Image from "next/image";
+
+
+
+
+export default function ImageSlider() {
+  return (
+    <Swiper
+      modules={[Autoplay]} 
+      effect="fade"
+      fadeEffect={{ crossFade: true }}
+      autoplay={{ delay: 3500, disableOnInteraction: false }}
+      loop={true} 
+      className="w-full h-[510px] " 
+      style={{ transitionTimingFunction: "ease-in-out" }} 
+    >
+      <SwiperSlide>
+        <Image
+          src={Hero1}
+          alt="Ethiopian traditional catering platter for events in San Diego California"
+          className="w-full h-full object-cover bg-black "
+          fill
+          style={{
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
+        />
+        <div className="absolute inset-0 bg-black/50 pointer-events-none" />
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <Image
+          src={Hero2}
+          alt="Luxury modern wedding catering setup in San Diego"
+          className="w-full h-full object-cover"
+          fill
+          style={{
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
+        />
+        <div className="absolute inset-0 bg-black/50 pointer-events-none" />
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <Image
+          src={Hero3}
+          alt="event catering with Ethiopian cuisine in San Diego"
+          className="w-full h-full object-cover"
+          fill
+          style={{
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
+        />
+        <div className="absolute inset-0 bg-black/50 pointer-events-none" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image
+          src={Hero10}
+          alt="Slide 3"
+          className="w-full h-full object-cover"
+          fill
+          style={{
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
+        />
+        <div className="absolute inset-0 bg-black/50 pointer-events-none" />
+      </SwiperSlide>
+
+
+      
+    </Swiper>
+  );
+}
